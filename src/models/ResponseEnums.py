ada5a6a2fc72f8d8127478da96521f4cb6c9ad1b
Ahmedkhaledd1 <ahmedkhaledno1@gmail.com>
2026-01-06 01:14:19 +0200

    add ResponseStatus file in model

---
+++ src/models/ResponseEnums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class ResponseStatus(Enum):
+    SUCCESS = "success"
+    FILE_TYPE_NOT_SUPPORTED = "file type not supported"
+    FILE_SIZE_EXCEEDED = "file size exceeded"
+    FILE_UPLOAD_ERROR = "file upload error"
