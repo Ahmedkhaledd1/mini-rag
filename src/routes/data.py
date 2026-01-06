0ef3ee3418892bc3fd0e65dcce773e11b0742eae
Ahmedkhaledd1 <ahmedkhaledno1@gmail.com>
2026-01-06 19:39:08 +0200

    implement uploading files logic

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,8 +1,14 @@
 
+import aiofiles
 from fastapi import FastAPI,APIRouter,Depends,UploadFile,status
 from helpers.config import Settings, get_settings
 from fastapi.responses import JSONResponse
-from controllers import DataController
+from controllers import DataController,ProjectController
+from models import ResponseStatus
+import logging
+import os
+
+logger=logging.getLogger("uvicorn.error")
 
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -18,3 +24,20 @@ async def upload_data(project_id: str,file: UploadFile,
             status_code=status.HTTP_400_BAD_REQUEST, 
             content={"status":message}
             )
+    
+    file_path=DataController().generate_unique_filename(file.filename,project_id)
+    try:
+        async with aiofiles.open(file_path, 'wb') as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+        logger.error(f"File upload failed: {e}")
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"status":ResponseStatus.FILE_UPLOAD_FAILED.value}
+            )
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"status":message}
+        )
